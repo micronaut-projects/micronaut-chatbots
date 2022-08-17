3ce715d442d47b3f02858dfb5a81e08aa7156649
Sergio del Amo <sergio.delamo@softamo.com>
2022-08-17 07:10:20 +0200

    build: disable binary compatibility

--- chatbots-bom/build.gradle.kts
+++ chatbots-bom/build.gradle.kts
@@ -1,3 +1,10 @@
 plugins {
     id("io.micronaut.build.internal.bom")
 }
+micronautBuild {
+    // new module, so no binary check
+    binaryCompatibility {
+        enabled.set(false)
+    }
+}
+tasks.named("checkVersionCatalogCompatibility").configure { enabled = false }
