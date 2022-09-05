cf108d0c94f9f49f645134254359c2b2209351d8
Tim Yates <tim.yates@gmail.com>
2022-09-05 13:46:34 +0100

    Try again for sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,12 @@ plugins {
     id("io.micronaut.build.internal.dependency-updates")
     id("io.micronaut.build.internal.quality-reporting")
 }
+
+if (System.getenv("SONAR_TOKEN") != null) {
+    configure<org.sonarqube.gradle.SonarQubeExtension> {
+        properties {
+            // Ignore duplication on the api classes as they are DTOs for Serde serialization
+            property("sonar.cpd.exclusions", "io/micronaut/chatbots/telegram/api/**")
+        }
+    }
+}
