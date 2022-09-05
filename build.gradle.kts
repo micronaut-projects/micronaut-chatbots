34cd55726a567e0e313357de8b9fd39032c917dd
Tim Yates <tim.yates@gmail.com>
2022-09-05 13:44:11 +0100

    Remove sonar duplication config (which doesn't work)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,3 @@ plugins {
     id("io.micronaut.build.internal.dependency-updates")
     id("io.micronaut.build.internal.quality-reporting")
 }
-
-if (System.getenv("SONAR_TOKEN") != null) {
-    sonarqube {
-        properties {
-            // Ignore duplication on the api classes as they are DTOs for Serde serialization
-            property("sonar.cpd.exclusions", "io/micronaut/chatbots/telegram/api/**")
-        }
-    }
-}
