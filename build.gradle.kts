7c028832c238efe73bcf0b010dff5b3c647bd809
Tim Yates <tim.yates@gmail.com>
2022-09-05 13:36:58 +0100

    Ignore duplication on API DTOs

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,8 @@ plugins {
 if (System.getenv("SONAR_TOKEN") != null) {
     sonarqube {
         properties {
-            property "sonar.exclusions", "**/example/**"
+            // Ignore the api classes as they are DTOs for Serde serialization
+            property "sonar.cpd.exclusions", "io/micronaut/chatbots/telegram/api/**"
         }
     }
 }
