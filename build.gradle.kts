081a6f8a15099e2137def3161948c05304c4a658
Tim Yates <tim.yates@gmail.com>
2022-09-05 13:49:35 +0100

    Try again again for sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ if (System.getenv("SONAR_TOKEN") != null) {
     configure<org.sonarqube.gradle.SonarQubeExtension> {
         properties {
             // Ignore duplication on the api classes as they are DTOs for Serde serialization
-            property("sonar.cpd.exclusions", "io/micronaut/chatbots/telegram/api/**")
+            property("sonar.cpd.exclusions", "**/io/micronaut/chatbots/telegram/api/**")
         }
     }
 }
