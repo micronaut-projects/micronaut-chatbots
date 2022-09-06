9d2675567f2e28288761ea244f4e897b9b73fd73
Sergio del Amo <sergio.delamo@softamo.com>
2022-09-06 18:34:00 +0200

    license and javadoc

--- chatbots-telegram-azure-functions/src/main/java/io/micronaut/chatbots/telegram/azurefunctions/Handler.java
+++ chatbots-telegram-azure-functions/src/main/java/io/micronaut/chatbots/telegram/azurefunctions/Handler.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2022 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.micronaut.chatbots.telegram.azurefunctions;
 
 import com.microsoft.azure.functions.HttpStatus;
@@ -18,6 +33,11 @@ import jakarta.inject.Inject;
 
 import java.util.Optional;
 
+/**
+ * Azure function to handle Telegram Webhook.
+ * @author Sergio del Amo
+ * @since 1.0.0
+ */
 public class Handler extends AzureFunction {
 
     @Inject
@@ -26,6 +46,12 @@ public class Handler extends AzureFunction {
     @Inject
     Dispatcher<TelegramBotConfiguration, Update, Send> dispatcher;
 
+    /**
+     *
+     * @param request Request
+     * @param context Context
+     * @return Webhook response
+     */
     @FunctionName("TelegramTrigger")
     public HttpResponseMessage handle(
         @HttpTrigger(
