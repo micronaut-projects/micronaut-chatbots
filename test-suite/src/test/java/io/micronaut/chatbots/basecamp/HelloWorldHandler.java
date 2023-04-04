ae3784aa281fe2490d0dcf5fe7654aeedaf73452
Sergio del Amo <sergio.delamo@softamo.com>
2023-04-04 11:54:25 +0200

    add missing import

--- test-suite/src/test/java/io/micronaut/chatbots/basecamp/HelloWorldHandler.java
+++ test-suite/src/test/java/io/micronaut/chatbots/basecamp/HelloWorldHandler.java
@@ -4,7 +4,7 @@ import io.micronaut.chatbots.basecamp.api.Query;
 import io.micronaut.chatbots.basecamp.core.BasecampBotConfiguration;
 import io.micronaut.chatbots.basecamp.core.BasecampHandler;
 import jakarta.inject.Singleton;
-
+import io.micronaut.core.annotation.NonNull;
 import javax.validation.constraints.NotNull;
 import java.util.Optional;
 
