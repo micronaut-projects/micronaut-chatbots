c010e1c64b8ef722622ebd17d4352e39a878ffc6
Tim Yates <tim.yates@gmail.com>
2023-11-27 12:15:23 +0000

    Remove unused empty string

--- chatbots-telegram-api/src/main/java/io/micronaut/chatbots/telegram/api/InputMedia.java
+++ chatbots-telegram-api/src/main/java/io/micronaut/chatbots/telegram/api/InputMedia.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2022 original authors
+ * Copyright 2017-2023 original authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@ import io.micronaut.core.annotation.Nullable;
 import io.micronaut.serde.annotation.Serdeable;
 
 import jakarta.validation.constraints.NotBlank;
+
 import java.util.List;
 
 /**
@@ -32,6 +33,7 @@ import java.util.List;
  */
 @Serdeable
 public abstract class InputMedia {
+
     /**
      * Type of the result, must be photo.
      */
@@ -158,10 +160,9 @@ public abstract class InputMedia {
 
     @Override
     public String toString() {
-        return "" +
-                ", type='" + type + '\'' +
-                ", media='" + media + '\'' +
-                ", caption='" + caption + '\'' +
-                ", parseMode='" + parseMode + '\'';
+        return ", type='" + type + '\'' +
+            ", media='" + media + '\'' +
+            ", caption='" + caption + '\'' +
+            ", parseMode='" + parseMode + '\'';
     }
 }
