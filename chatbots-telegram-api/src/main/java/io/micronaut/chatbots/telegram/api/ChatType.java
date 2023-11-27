b1ef3dc07fde4bcef4f8f8222dcc17c35c68893e
Tim Yates <tim.yates@gmail.com>
2023-11-27 12:29:00 +0000

    CheckStyle issue (blank line above CTOR)

--- chatbots-telegram-api/src/main/java/io/micronaut/chatbots/telegram/api/ChatType.java
+++ chatbots-telegram-api/src/main/java/io/micronaut/chatbots/telegram/api/ChatType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2021 original authors
+ * Copyright 2017-2023 original authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,7 @@ public enum ChatType {
     CHANNEL("channel");
 
     private String value;
+
     ChatType(String value) {
         this.value = value;
     }
