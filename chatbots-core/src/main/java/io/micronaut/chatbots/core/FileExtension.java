252bc2bfdfd3f9033a59b3feeb5577fb86d2d695
Tim Yates <tim.yates@gmail.com>
2023-11-27 11:40:50 +0000

    Checkstyle fix

--- chatbots-core/src/main/java/io/micronaut/chatbots/core/FileExtension.java
+++ chatbots-core/src/main/java/io/micronaut/chatbots/core/FileExtension.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2022 original authors
+ * Copyright 2017-2023 original authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,9 @@ public enum FileExtension {
     MARKDOWN("md", "markdown"),
     HTML("html"),
     TXT("txt");
+
     private final String[] extensions;
+
     FileExtension(String... extensions) {
         this.extensions = extensions;
     }
