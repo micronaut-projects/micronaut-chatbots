00a6ab30c1fc303e4d7ed585cb6999899021ee7f
issam liqali <liqaliissam@gmail.com>
2025-12-12 16:07:16 +0100

    add spock version

--- test-suite-groovy/build.gradle.kts
+++ test-suite-groovy/build.gradle.kts
@@ -24,3 +24,10 @@ java {
     sourceCompatibility = JavaVersion.toVersion("21")
     targetCompatibility = JavaVersion.toVersion("21")
 }
+
+//TODO remove once Micronaut Test ships Spock version compatible with Groovy 5
+configurations.all {
+    resolutionStrategy {
+        force("org.spockframework:spock-core:2.4-M7-groovy-5.0")
+    }
+}
